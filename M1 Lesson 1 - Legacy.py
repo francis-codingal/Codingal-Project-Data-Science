6ea44b75ee322ac6949629b3ee88c4fccea98c09
francis-codingal <francissantos.codingal@gmail.com>
2026-07-09 15:38:19 +0530

    ds

---
+++ M1 Lesson 1 - Legacy.py
@@ -0,0 +1,13 @@
+#Print a Sentence 
+print("Let's explore the world of programming!")
+
+#Print a Number
+print(7)
+
+#Next Line after print
+print("Hello Python \n")
+
+#Print all the keywords present in Python
+print("Keywords in Python are....")
+import keyword
+print(keyword.kwlist)
