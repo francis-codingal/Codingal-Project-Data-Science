d6ba7b82f12db4a0094f508a757951a24e7e21f6
francis-codingal <francissantos.codingal@gmail.com>
2026-07-15 16:06:00 +0530

    m2

---
+++ M2 Lesson 3 - Class student 2.py
@@ -0,0 +1,14 @@
+class student:
+	grade = 12
+	name = "Francis"
+	
+	def introduction(self):
+		print("Hi I am a student")
+
+	def details(self):
+		print("My name is", self.name)
+		print("I study in Grade", self.grade)
+
+ob = student()
+ob.introduction()
+ob.details()
